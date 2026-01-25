eb02a4700ad6303429427bfe70e89acfce6d3b35
Sam Lin <maxisam@gmail.com>
2026-01-25 17:41:50 -0600

    feat: add AppComponent with game store integration and service worker update logic.

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,8 +1,10 @@
 
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameStore } from './services/game.store';
 import { MoleComponent } from './components/mole.component';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,22 @@ import { MoleComponent } from './components/mole.component';
   imports: [CommonModule, MoleComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   store = inject(GameStore);
+  swUpdate = inject(SwUpdate);
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates
+        .pipe(filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'))
+        .subscribe(() => {
+          if (confirm('New version available. Load New Version?')) {
+            // Activate the new version and reload the page
+            this.swUpdate.activateUpdate().then(() => document.location.reload());
+          }
+        });
+    }
+  }
 
   // Helper to get moles as array for template loop
   get moles() {
